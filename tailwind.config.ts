8ac1f107fd350f7263189aad3e7f0f00c14148b3
heejae <leeheejaelcode@gmail.com>
2024-10-22 19:28:42 +0900

    [환경구성] 대표색상 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,15 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: "#2f2f2f",
+        primary: "#674188",
       },
     },
   },
