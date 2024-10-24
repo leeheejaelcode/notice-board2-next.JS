dc6cd4523ec75e3cef2897f1223202ebcf389876
heejae <leeheejaelcode@gmail.com>
2024-10-24 17:37:51 +0900

    [생성] not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center main">
+      <h1 className="font-bold text-slate-500 text-[40px]">
+        페이지를 찾을수 없습니다.
+      </h1>
+    </div>
+  );
+}
